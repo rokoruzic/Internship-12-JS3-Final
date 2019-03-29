bb30616ceb6cc7d08194ecc818b88b623d0317d9
rokoruzic <roko.ruzic4@gmail.com>
2019-03-29 16:39:19 +0100

    added css for login and register

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,48 @@
 $(document).ready(function () {
 if(localStorage.getItem('user')===null)
 
-    $("#registration").show();
+    $('#registration__container').show();
     else
-    $("#login").show();
+    $('#login__container').show();
 
 
-    $("#signUpButton").click(function () {
+    $('#signUpButton').click(function () {
       
         const user = {
             username: $('#inputUsername').val(),
             password: $('#inputPassword').val()
         };
         if (user.username.length < 5 || user.password < 5)
-            alert("Username or password too short")
+            alert('Username or password too short')
         else if (user.password !== $('#inputRepeatedPassword').val())
-            alert("Passwords don't match")
+            alert('Passwords dont match')
         else
         {
             localStorage.setItem('user',JSON.stringify(user));
-            $("#registration").hide();
-            $("#login").show();
+            $('#registration__container').hide();
+            $('#login__container').show();
 
         }
     });
-    $("#signInButton").click(function () {
+    $('#signInButton').click(function () {
         const userInfo = JSON.parse( localStorage.getItem('user') );
         if(userInfo.username !== $('#loginUsername').val() || userInfo.password !== $('#loginPassword').val())
-        alert("Username or password not correct");
+        alert('Username or password not correct');
         else
         {
-        $("#login").hide();
-        $("#users").show();
+        $('#login__container').hide();
+        $('#users').show();
         }
     });
     $.get('https://jsonplaceholder.typicode.com/users', function (userData) {
         console.log(userData);
-        $(".item__paragraph").each(function (i) {
+        $('.item__paragraph').each(function (i) {
             $(this).html(`<br>${userData[i].name}</br> ${userData[i].email}`);
 
          
         });
 
-        $(".item__price").each(function (i) {
+        $('.item__price').each(function (i) {
             $(this).html(userData[i].address.city);
         });
         
@@ -53,25 +53,25 @@ if(localStorage.getItem('user')===null)
         $('.offers__item').each(function (i) {
             
             $(this).click(function () {
-                $("#popup__item").show();
+                $('#popup__item').show();
                 $('#user__details').html(`<h1>Name: ${userData[i].name}</h1> <p>Email: ${userData[i].email}</p> 
               <p>Phone: ${userData[i].phone}</p> <p>Web: ${userData[i].website}</p> <p> Username: ${userData[i].username} <br> </br> <h2>Adress:</h2>
                <p>Street: ${userData[i].address.street} </p> <p> Suite: ${userData[i].address.suite}</p> <p>City: ${userData[i].address.city}</p> <p> Zipcode: ${userData[i].address.zipcode} </p>
                <br> </br> <h2> Company: </h2> <p>Name: ${userData[i].company.name}</p> <p> Phrase: ${userData[i].company.catchPhrase}
                <p> Bs: ${userData[i].company.bs} </p>`
                 );
-                $("#popup__item").scroll(function () {
+                $('#popup__item').scroll(function () {
                     if ($(this).scrollTop() > 75)
-                        $('.scroll__to__top__button').css("display", "block");
+                        $('.scroll__to__top__button').css('display', 'block');
                     else
-                        $('.scroll__to__top__button').css("display", "none");
+                        $('.scroll__to__top__button').css('display', 'none');
                 });
                 $('.scroll__to__top__button').click(function () {
                     $('#popup__item').scrollTop(0);
                 })
             });
             $('.close__button').click(function () {
-                $("#popup__item").hide();
+                $('#popup__item').hide();
 
             })
         });
@@ -82,18 +82,18 @@ if(localStorage.getItem('user')===null)
             $('.posts__button').each(function (i) {
                 $(this).click(function (event) {
                     let userPosts = postData.filter(x => x.userId == $('.offers__item').eq(i).attr('userId'));
-                    $("#user__details").append(`<h1>UserId: ${$('.offers__item').eq(i).attr('userId')} <br> </br>`);
+                    $('#user__details').append(`<h1>UserId: ${$('.offers__item').eq(i).attr('userId')} <br> </br>`);
                     userPosts.forEach(function (post) {
-                        $("#user__details").append(`<h1> ${post.title} </h1>
+                        $('#user__details').append(`<h1> ${post.title} </h1>
                         <p>PostId: ${post.id}</p> <p>Post: ${post.body} </p> <br> </br> `);
                     });
-                    $("#popup__item").show();
+                    $('#popup__item').show();
 
-                    $("#popup__item").scroll(function () {
+                    $('#popup__item').scroll(function () {
                         if ($(this).scrollTop() > 75)
-                            $('.scroll__to__top__button').css("display", "block");
+                            $('.scroll__to__top__button').css('display', 'block');
                         else
-                            $('.scroll__to__top__button').css("display", "none");
+                            $('.scroll__to__top__button').css('display', 'none');
                     });
                     $('.scroll__to__top__button').click(function () {
                         $('#popup__item').scrollTop(0);
@@ -101,7 +101,7 @@ if(localStorage.getItem('user')===null)
                     event.stopPropagation();
                 });
                 $('.close__button').click(function () {
-                    $("#popup__item").hide();
+                    $('#popup__item').hide();
                     $('#user__details').html('');
 
                 })
@@ -110,11 +110,11 @@ if(localStorage.getItem('user')===null)
         });
         $('.new__post__button').each(function (i) {
         $(this).click(function (event) {
-            $("#submit__post").show();
-            $("#submit__post").attr('userId',$('.offers__item').eq(i).attr('userId'));
+            $('#submit__post').show();
+            $('#submit__post').attr('userId',$('.offers__item').eq(i).attr('userId'));
             event.stopPropagation();
             $('.close__button').click(function () {
-                $("#submit__post").hide();
+                $('#submit__post').hide();
                 $('#post__title').val('');
                 $('#post__body').val('');
 
@@ -126,25 +126,25 @@ if(localStorage.getItem('user')===null)
             var post = {
                 title: $('#post__title').val(),
                 body: $('#post__body').val(),
-                userId: $("#submit__post").attr('userId')
+                userId: $('#submit__post').attr('userId')
 
             };
             if(post.title.length<1 || post.body.length<1)
-            alert("Title and/or body musn't be empty");
+            alert('Title and/or body musnt be empty');
             else
             {
-             $("#submit__post").hide();
+             $('#submit__post').hide();
             $.ajax({
-                type: "POST",
-                url: "https://jsonplaceholder.typicode.com/posts",
+                type: 'POST',
+                url: 'https://jsonplaceholder.typicode.com/posts',
                 data: JSON.stringify(post),
-                contentType: "application/json; charset=utf-8",
-                dataType: "json",
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
                 success: function(){
-                    alert("Success!")
+                    alert('Success!')
                     ;},
                 failure: function() {
-                    alert("Failure!");
+                    alert('Failure!');
                 }
             });
            
@@ -158,39 +158,3 @@ if(localStorage.getItem('user')===null)
     });
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
